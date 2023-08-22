4c6e75fa853b11e601850d8a6189a6393d31c92a
matidominati <mateuszdermont@gmail.com>
2023-08-22 22:31:05 +0200

    PersonDisplayInfo

---
+++ src/main/java/com/dermont/ResidentialInfo/usableArea.java
@@ -0,0 +1,50 @@
+package com.dermont.ResidentialInfo;
+
+public class usableArea {
+    private double usableAreaLength;
+    private double usableAreaWidth;
+    private double usableAreaHeight;
+    private double usableAreaCapacity;
+
+    public usableArea(double usableAreaLength, double usableAreaWidth, double usableAreaHeight) {
+        this.usableAreaLength = usableAreaLength;
+        this.usableAreaWidth = usableAreaWidth;
+        this.usableAreaHeight = usableAreaHeight;
+    }
+
+    public usableArea(double usableAreaCapacity) {
+        this.usableAreaCapacity = usableAreaCapacity;
+    }
+
+    public double getUsableAreaLength() {
+        return usableAreaLength;
+    }
+
+    public void setUsableAreaLength(double usableAreaLength) {
+        this.usableAreaLength = usableAreaLength;
+    }
+
+    public double getUsableAreaWidth() {
+        return usableAreaWidth;
+    }
+
+    public void setUsableAreaWidth(double usableAreaWidth) {
+        this.usableAreaWidth = usableAreaWidth;
+    }
+
+    public double getUsableAreaHeight() {
+        return usableAreaHeight;
+    }
+
+    public void setUsableAreaHeight(double usableAreaHeight) {
+        this.usableAreaHeight = usableAreaHeight;
+    }
+
+    public double getUsableAreaCapacity() {
+        return usableAreaCapacity;
+    }
+
+    public void setUsableAreaCapacity(double usableAreaCapacity) {
+        this.usableAreaCapacity = usableAreaCapacity;
+    }
+}
